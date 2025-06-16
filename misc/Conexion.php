e5d355993afb26aa62ecad0fd56ea57716cf9fb2
Khris_Rod <rodriguezchris808@gmail.com>
2025-06-15 21:49:01 -0600

    ..

--- misc/Conexion.php
+++ misc/Conexion.php
@@ -1,37 +1,32 @@
-
-<?php 
-
-
-    class Conexion {
-
-        public static function conectar(){
-            // $host = 'srv863.hstgr.io';
-            // $db = 'u484426513_ms125';
-            // $user = 'u484426513_ms125';
-            // $pass = 'N]18zvue';
-            // $charset = 'utf8mb4';
-             $host = '127.0.0.1';
-            $db = 'fff';
-            $user = 'root';
-            $pass = 'Cris270814';
-            $charset = 'utf8mb4';
+<?php
 
 
-            $dsn = "mysql:host=$host; dbname=$db; charset=$charset";
+class Conexion
+{
+
+    public static function conectar()
+    {
+        $host = 'srv863.hstgr.io';
+        $db = 'u484426513_ms125';
+        $user = 'u484426513_ms125';
+        $pass = 'N]18zvue';
+        $charset = 'utf8mb4';
 
 
-            try{
-                $pdo = new PDO( $dsn, $user, $pass ); 
-                $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                return $pdo;  
-            }
-            catch (PDOException $e){
-                 die("Error de conexion: ". $e->getMessage());
-            }
+        $dsn = "mysql:host=$host; dbname=$db; charset=$charset";
+
+
+        try {
+            $pdo = new PDO($dsn, $user, $pass);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (PDOException $e) {
+            die("Error de conexion: " . $e->getMessage());
         }
-
-
     }
+
+
+}
 
 
 
