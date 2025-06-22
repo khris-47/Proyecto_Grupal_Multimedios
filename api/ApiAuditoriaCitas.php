2462f3117dac80e6862e1ee43b2c1e7abbfa2b8e
Khris_Rod <rodriguezchris808@gmail.com>
2025-06-21 20:00:56 -0600

    cambios en API, permitir consumo en react

--- api/ApiAuditoriaCitas.php
+++ api/ApiAuditoriaCitas.php
@@ -1,5 +1,16 @@
 <?php
 
+// Permitir CORS para desarrollo
+header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Methods: GET,  PTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Responder a la petición OPTIONS (preflight)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 require_once __DIR__ . '/../controller/AuditoriaCitaController.php';
 
 class ApiAuditoriaCitas {
--- api/ApiUsuarios.php
+++ api/ApiUsuarios.php
@@ -1,5 +1,17 @@
 <?php
 
+// Permitir CORS para desarrollo
+header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Methods: POST, GET, DELETE, PUT,  OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Responder a la petición OPTIONS (preflight)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+
 // Requerimos las clases necesarias para trabajar con usuarios desde su DAO y modelo
 require_once __DIR__ . '/../accessData/UsuariosDAO.php';
 require_once __DIR__ . '/../model/Usuario.php';
